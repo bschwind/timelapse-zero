8f75c3c9cd32bb3f76cee25769b94e578ddcd2c4
Brian Schwind <brianmschwind@gmail.com>
2017-01-07 21:24:04 +0900

    Update camera settings

--- camera.sh
+++ camera.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 DATE=$(date +%s)
-raspistill -o /home/pi/camera/$DATE.png -t 1
+raspistill -o /home/pi/camera/$DATE.png \
+           -t 1 \
+           -q 100 \
+           -ISO 100 \
+           -awb horizon \
+           --metering spot
