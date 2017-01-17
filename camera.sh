57870d52b6d3613ba4af7959d6aba69171642df2
Brian Schwind <brianmschwind@gmail.com>
2017-01-17 16:47:45 +0900

    Update camera settings

--- camera.sh
+++ camera.sh
@@ -3,8 +3,8 @@ DATE=$(date +"%Y_%m_%d")
 TIME=$(date +%s)
 mkdir -p /home/pi/camera/$DATE
 raspistill -o /home/pi/camera/$DATE/$TIME.jpg \
-           -t 1 \
+           -t 3000 \
            -q 100 \
            -ISO 100 \
-           -awb horizon \
-           --metering spot
+           -awb cloud \
+           --metering matrix
